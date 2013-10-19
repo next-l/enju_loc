5e2bce68926b7fc014f781569207807c0085b53b
Masao Takaku <tmasao@acm.org>
2013-10-19 16:03:28 +0900

    add link_to_import helper method.

--- app/helpers/loc_search_helper.rb
+++ app/helpers/loc_search_helper.rb
@@ -1,2 +1,14 @@
 module LocSearchHelper
+  def link_to_import(lccn)
+    if lccn.blank?
+      t('enju_loc.not_available')
+    else
+      manifestation = Manifestation.where(:lccn => lccn).first
+      unless manifestation
+        link_to t('enju_loc.add'), ndl_books_path(:book => {:lccn => lccn}), :method => :post
+      else
+        link_to t('enju_loc.already_exists'), manifestation
+      end
+    end
+  end
 end
