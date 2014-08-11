ae97206072276be7c7d332d2a7b55e0fb2a399f0
Masao Takaku <tmasao@acm.org>
2014-08-11 07:07:51 +0000

    add contraller spec.

---
+++ spec/controllers/loc_search_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe LocSearchController do
+  fixtures :all
+
+  it "should be a kind of enju_loc" do
+    assert_kind_of Module, EnjuLoc
+  end
+
+  describe "GET index" do
+    login_fixture_librarian
+
+    it "should get index", :vcr => true do
+      get :index, :query => 'library'
+      expect( assigns(:books) ).not_to be_empty
+    end
+
+    it "should be empty if a query is not set", :vcr => true do
+      get :index
+      expect( assigns(:books) ).to be_empty
+    end
+  end
+
+  describe "POST create" do
+    login_fixture_librarian
+
+    it "should create a bibliographic record if lccn is set", :vcr => true do
+      post :create, :book => {:lccn => '2013385616'}
+      expect( assigns(:manifestation).identifier_contents(:lccn) ).to eq ['2013385616']
+      expect( response ).to redirect_to manifestation_items_url( assigns(:manifestation) )
+    end
+
+    it "should not create a bibliographic record if lccn is not set", :vcr => true do
+      post :create, :book => {}
+      expect( assigns(:manifestation) ).to be_nil
+      expect( response ).to redirect_to loc_search_index_url
+    end
+  end
+end
