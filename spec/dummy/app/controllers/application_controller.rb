9d49428a0ac137c9500f75a987ac9ae50d03cfaf
Kosuke Tanabe <nabeta@fastmail.fm>
2016-01-24 04:43:36 -0500

    update dummy files

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include EnjuLeaf::Controller
+  include EnjuBiblio::Controller
+  include EnjuLibrary::Controller
+  after_action :verify_authorized
+
+  include Pundit
 end
