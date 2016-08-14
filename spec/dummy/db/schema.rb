dab1fa8d29c8c5e497c79eaa1a9d3a0b91579602
Kosuke Tanabe <nabeta@fastmail.fm>
2016-08-14 16:46:49 +0900

    update dummy migration files

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141003182825) do
+ActiveRecord::Schema.define(version: 20160813203039) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer  "basket_id"
@@ -234,6 +234,17 @@ ActiveRecord::Schema.define(version: 20141003182825) do
   add_index "classifications", ["manifestation_id"], name: "index_classifications_on_manifestation_id"
   add_index "classifications", ["parent_id"], name: "index_classifications_on_parent_id"
 
+  create_table "colors", force: :cascade do |t|
+    t.integer  "library_group_id"
+    t.string   "property"
+    t.string   "code"
+    t.integer  "position"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "colors", ["library_group_id"], name: "index_colors_on_library_group_id"
+
   create_table "content_types", force: :cascade do |t|
     t.string   "name",         null: false
     t.text     "display_name"
@@ -449,10 +460,22 @@ ActiveRecord::Schema.define(version: 20141003182825) do
   add_index "libraries", ["library_group_id"], name: "index_libraries_on_library_group_id"
   add_index "libraries", ["name"], name: "index_libraries_on_name", unique: true
 
+  create_table "library_group_translations", force: :cascade do |t|
+    t.integer  "library_group_id", null: false
+    t.string   "locale",           null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.text     "login_banner"
+    t.text     "footer_banner"
+  end
+
+  add_index "library_group_translations", ["library_group_id"], name: "index_library_group_translations_on_library_group_id"
+  add_index "library_group_translations", ["locale"], name: "index_library_group_translations_on_locale"
+
   create_table "library_groups", force: :cascade do |t|
-    t.string   "name",                                              null: false
+    t.string   "name",                                                             null: false
     t.text     "display_name"
-    t.string   "short_name",                                        null: false
+    t.string   "short_name",                                                       null: false
     t.text     "my_networks"
     t.text     "login_banner"
     t.text     "note"
@@ -461,10 +484,17 @@ ActiveRecord::Schema.define(version: 20141003182825) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "admin_networks"
-    t.string   "url",            default: "http://localhost:3000/"
+    t.string   "url",                           default: "http://localhost:3000/"
+    t.text     "settings"
+    t.text     "html_snippet"
+    t.integer  "max_number_of_results",         default: 500
+    t.boolean  "family_name_first",             default: true
+    t.integer  "pub_year_facet_range_interval", default: 10
+    t.integer  "user_id"
   end
 
   add_index "library_groups", ["short_name"], name: "index_library_groups_on_short_name"
+  add_index "library_groups", ["user_id"], name: "index_library_groups_on_user_id"
 
   create_table "licenses", force: :cascade do |t|
     t.string   "name",         null: false
@@ -910,6 +940,7 @@ ActiveRecord::Schema.define(version: 20141003182825) do
     t.integer  "user_export_file_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "most_recent"
   end
 
   add_index "user_export_file_transitions", ["sort_key", "user_export_file_id"], name: "index_user_export_file_transitions_on_sort_key_and_file_id", unique: true
@@ -955,6 +986,7 @@ ActiveRecord::Schema.define(version: 20141003182825) do
     t.integer  "user_import_file_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "most_recent"
   end
 
   add_index "user_import_file_transitions", ["sort_key", "user_import_file_id"], name: "index_user_import_file_transitions_on_sort_key_and_file_id", unique: true
@@ -966,7 +998,7 @@ ActiveRecord::Schema.define(version: 20141003182825) do
     t.datetime "executed_at"
     t.string   "user_import_file_name"
     t.string   "user_import_content_type"
-    t.string   "user_import_file_size"
+    t.integer  "user_import_file_size"
     t.datetime "user_import_updated_at"
     t.string   "user_import_fingerprint"
     t.string   "edit_mode"
@@ -984,6 +1016,7 @@ ActiveRecord::Schema.define(version: 20141003182825) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "error_message"
   end
 
   create_table "users", force: :cascade do |t|
@@ -1023,5 +1056,16 @@ ActiveRecord::Schema.define(version: 20141003182825) do
   end
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
+
+  create_table "withdraws", force: :cascade do |t|
+    t.integer  "basket_id"
+    t.integer  "item_id"
+    t.integer  "librarian_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "withdraws", ["basket_id"], name: "index_withdraws_on_basket_id"
+  add_index "withdraws", ["item_id"], name: "index_withdraws_on_item_id"
 
 end
