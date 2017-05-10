2fb4e832a0dc8d0a0299c3bea680a561a8c9a978
Kosuke Tanabe <nabeta@fastmail.fm>
2017-05-10 13:20:36 +0900

    update Gemfile

--- app/models/lccn_record.rb
+++ app/models/lccn_record.rb
@@ -1,3 +1,14 @@
 class LccnRecord < ApplicationRecord
   belongs_to :manifestation
 end
+
+# == Schema Information
+#
+# Table name: lccn_records
+#
+#  id               :integer          not null, primary key
+#  body             :string           not null
+#  manifestation_id :uuid             not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
