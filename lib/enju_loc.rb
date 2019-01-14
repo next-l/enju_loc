a86c520b573788811839530531c3dc79341466f1
Kosuke Tanabe <nabeta@fastmail.fm>
2019-01-14 23:38:51 +0900

    move module

--- lib/enju_loc.rb
+++ lib/enju_loc.rb
@@ -1,21 +1,4 @@
 require "enju_loc/engine"
-require "enju_loc/loc_search"
 
 module EnjuLoc
-  module ActsAsMethods
-    def self.included(base)
-      base.extend ClassMethods
-    end
-
-    module ClassMethods
-      def enju_loc_search
-        include EnjuLoc::LocSearch
-      end
-    end
-  end
-
-  class RecordNotFound < StandardError; end
-  class InvalidIsbn < StandardError; end
 end
-
-ActiveRecord::Base.send :include, EnjuLoc::ActsAsMethods
--- spec/dummy/db/migrate/124_create_bookstores.rb
+++ spec/dummy/db/migrate/124_create_bookstores.rb
@@ -1,7 +1,7 @@
-class CreateBookstores < ActiveRecord::Migration[4.2]
+class CreateBookstores < ActiveRecord::Migration[5.2]
   def change
     create_table :bookstores do |t|
-      t.text :name, :null => false
+      t.text :name, null: false
       t.string :zip_code
       t.text :address
       t.text :note
@@ -9,7 +9,6 @@ class CreateBookstores < ActiveRecord::Migration[4.2]
       t.string :fax_number
       t.string :url
       t.integer :position
-      t.datetime :deleted_at
 
       t.timestamps
     end
