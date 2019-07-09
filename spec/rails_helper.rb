dcab9c104c7a1e56ca90af2be59cd5eff1b09c3d
Kosuke Tanabe <nabeta@fastmail.fm>
2019-07-10 00:16:15 +0900

    update spec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,8 +13,6 @@ require 'vcr'
 # in spec/support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
-$original_sunspot_session = Sunspot.session
-
 RSpec.configure do |config|
   # == Mock Framework
   #
